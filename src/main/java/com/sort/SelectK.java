8e21654cffd0e1046df4d7f5991e38b1f105684c
zicheng.pan <1094275633@qq.com>
2023-03-27 11:04:59 +0800

    fix partition bug for select k and add irrecursive

--- src/main/java/com/sort/SelectK.java
+++ src/main/java/com/sort/SelectK.java
@@ -1,19 +1,40 @@
 package com.sort;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class SelectK {
 
 
     public static <E extends Comparable<E>> void main(String[] args) {
-        int k = 4;
-        Integer[] array = new Integer[]{1, 6, 4, 2, 5, 3};
+        int k = 7-1;
+        // 1 2 3 4 5 6 9 10
+        Integer[] array = new Integer[]{1,3, 6, 4, 2, 5, 3, 9, 10};
+        Integer[] arrayCopy = Arrays.copyOf(array, array.length);
         Random random = new Random();
         int result = SelectK.selectK(array, 0, array.length - 1, k, random);
+        Integer result_copy = SelectK.selectKselectKwithoutRecursive(arrayCopy, k, random);
         System.out.println(result);
+        System.out.println(result_copy);
     }
 
-    private static <E extends Comparable<E>> E selectK(E[] array, int left, int right, int k, Random random) {
+    public static <E extends Comparable<E>> E selectKselectKwithoutRecursive(E[] array, int k, Random random) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            int index = partition(array, left, right, random);
+            if (index == k) {
+                return array[index];
+            } else if (index < k) {
+                left = index + 1;
+            } else {
+                right = index - 1;
+            }
+        }
+        return null;
+    }
+
+    public static <E extends Comparable<E>> E selectK(E[] array, int left, int right, int k, Random random) {
 //        int left = 0;
 //        int right = array.length - 1;
         int index = partition(array, left, right, random);
@@ -26,13 +47,20 @@ public class SelectK {
         }
     }
 
+
     private static <E extends Comparable<E>> int partition(E[] array, int left, int right, Random random) {
-        int random_index = random.nextInt(right - left + 1);
-        swap(array, left, left + random_index);
+//        int random_index = random.nextInt(right - left + 1);
+//        swap(array, left, left + random_index);
         int l = left + 1;
         int r = right;
-        int j = left + 1;
         E e = array[left];
+        while (r > left) {
+            if (array[r].compareTo(e) < 0) {
+                break;
+            }
+            r--;
+        }
+        int j = left + 1;
         // left + 1 .. l < e
         // l .. j = e
         // r .. right > e
